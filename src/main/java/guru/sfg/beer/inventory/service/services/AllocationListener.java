5347ce3dd049ef3f7a66113e74cfced228f4d413
nivaldomonteiro <nushnivaldo@gmail.com>
2020-11-12 18:03:12 +0000

    Fixed allocation listener result property - closes #2

--- src/main/java/guru/sfg/beer/inventory/service/services/AllocationListener.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationListener.java
@@ -12,63 +12,35 @@ import org.springframework.stereotype.Component;
 /**
  * Created by jt on 12/3/19.
  */
-//@Slf4j
-//@RequiredArgsConstructor
-//@Component
-//public class AllocationListener {
-//    private final AllocationService allocationService;
-//    private final JmsTemplate jmsTemplate;
-//
-//    @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
-//    public void listen(AllocateOrderRequest request){
-//        AllocateOrderResult.AllocateOrderResultBuilder builder = AllocateOrderResult.builder();
-//        builder.beerOrderDto(request.getBeerOrderDto());
-//
-//        try{
-//            Boolean allocationResult = allocationService.allocateOrder(request.getBeerOrderDto());
-//
-//            if (allocationResult){
-//                builder.pendingInventory(false);
-//            } else {
-//                builder.pendingInventory(true);
-//            }
-//
-//            builder.allocationError(false);
-//        } catch (Exception e){
-//            log.error("Allocation failed for Order Id:" + request.getBeerOrderDto().getId());
-//            builder.allocationError(true);
-//        }
-//
-//        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE,
-//                builder.build());
-//
-//    }
-//}
-
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class AllocationListener {
-
-    private final JmsTemplate jmsTemplate;
     private final AllocationService allocationService;
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
-    public void listen(AllocateOrderRequest request) {
-
+    public void listen(AllocateOrderRequest request){
         AllocateOrderResult.AllocateOrderResultBuilder builder = AllocateOrderResult.builder();
         builder.beerOrderDto(request.getBeerOrderDto());
 
-        try {
-            final Boolean allAllocationResult = allocationService.allocateOrder(request.getBeerOrderDto());
-            if(allAllocationResult) {
+        try{
+            Boolean allocationResult = allocationService.allocateOrder(request.getBeerOrderDto());
+
+            if (allocationResult){
                 builder.pendingInventory(false);
+            } else {
+                builder.pendingInventory(true);
             }
-        } catch (Exception e) {
-            log.error("Allocation failed for Order Id: " + request.getBeerOrderDto().getId());
+
+            builder.allocationError(false);
+        } catch (Exception e){
+            log.error("Allocation failed for Order Id:" + request.getBeerOrderDto().getId());
             builder.allocationError(true);
         }
 
-        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE, builder.build());
+        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE,
+                builder.build());
+
     }
 }
